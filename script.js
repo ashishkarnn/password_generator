22385d5d78bdf078e4627c07f4bc7d182dea81e1
ashish-karnn <142218925+ashishkarnn@users.noreply.github.com>
2024-09-09 23:31:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const password_box = document.getElementById('password');
+const length = 12;
+
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const number = '0123456789';
+const symbol = '!@#$%^&*()_+-';
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = '';
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (password.length < length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    password_box.value = password;
+}
+
+function copy_password() {
+    password_box.select();
+    document.execCommand('copy');
+}
